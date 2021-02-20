2e94898d8b93e5ae747d407838cc0d0d8245641f
hippo_o_matic <hippo.o.matic@gmail.com>
2021-02-20 14:52:45 -0800

    Fix a scaling by 0 issue

--- src/mesh2d.cpp
+++ src/mesh2d.cpp
@@ -146,119 +146,17 @@ void Polygon::getPositionMinMax(const std::vector<glm::vec2>& positions, float* 
 	(*y_max) = _y_max;
 }
 
-// const std::vector<Vertex2d> genTexCoords2d(std::vector<float> v_positions, std::vector<glm::vec2> tex_basis, unsigned int attribs) {
-// 	std::vector<Vertex2d> vertices;
-
-// 	float x_min, x_max, y_min, y_max;
-// 	getPositionMinMax(v_positions, &x_min, &x_max, &y_min, &y_max);
-
-// 	// Construct vertexes with the same positions, and texcoords from a scale of 0 to 1
-// 	// depending on where the vertexes are relative to the polygon
-// 	for(auto it = v_positions.begin(); it != v_positions.end(); it++) {
-// 		float x = *it, y = *(++it);		
-
-// 		// The texture coordinate for this vertex
-// 		glm::vec3 texCoord = glm::vec3(
-// 			(x-x_min)/(x_max - x_min),
-// 			(y-y_min)/(y_max - y_min),
-// 			0
-// 		);
-// 		glm::mat4 tCoordMat = glm::translate(glm::mat4(1), texCoord); // Translate to this point
-// 		glm::mat4 offsetMat = glm::translate(glm::mat4(1), glm::vec3(.5, .5, 0)); // Translate so attrib operations are centered
-
-// 		glm::mat4 basisMat = glm::translate(glm::mat4(1), glm::vec3(tex_basis[0], 0)); // Move from the original origin to the new origin (lower left corner of basis) 
-// 		basisMat = glm::scale(basisMat, glm::vec3(tex_basis[1] - tex_basis[0], 0)); // Scale to the texBasis' size 
-
-// 		glm::mat4 attribMat = glm::mat4(1);
-// 		// Check attributes and perform their operations
-// 		if(attribs & ROTATE_90)
-// 			attribMat = glm::rotate(attribMat, glm::radians(90.f), glm::vec3(0, 0, 1));
-// 		if(attribs & ROTATE_180)
-// 			attribMat = glm::rotate(attribMat, glm::radians(180.f), glm::vec3(0, 0, 1));
-// 		if(attribs & REFLECT_V)
-// 			attribMat[1][1] *= -1;
-// 		if(attribs & REFLECT_H)
-// 			attribMat[0][0] *= -1;
-
-// 		attribMat = offsetMat * attribMat * glm::inverse(offsetMat);
-// 		tCoordMat = basisMat * attribMat * tCoordMat;
-		
-// 		vertices.push_back(
-// 			Vertex2d {
-// 				pos: glm::vec2(x, y),
-// 				tex: glm::vec2(tCoordMat[3])
-// 			}
-// 		);
-// 	}
-
-// 	return vertices;
-// }
-
-const std::vector<Vertex2d> joinCoords(std::vector<float> pos, std::vector<float> tex) {
-	std::vector<Vertex2d> result;
-	if(pos.size() != tex.size()) {
-		return result;
-	}
-	for(unsigned i=0; i<pos.size(); i+=2) {
-		result.push_back(Vertex2d {
-			pos: glm::vec2(pos[i], pos[i+1]),
-			tex: glm::vec2(tex[i], pos[i+1])
-		});
-	}
-
-	return result;
-}
-
-const std::vector<Vertex2d> joinCoords(std::vector<glm::vec2> pos, std::vector<glm::vec2> tex) {
-	std::vector<Vertex2d> result;
-	if(pos.size() != tex.size()) {
-		return result;
-	}
-	for(unsigned i=0; i<pos.size(); i++) {
-		result.push_back(Vertex2d {
-			pos: pos[i],
-			tex: tex[i]
-		});
-	}
-
-	return result;
-}
-
-const std::vector<Vertex2d> joinCoords(std::vector<Vertex2d> pos, std::vector<glm::vec2> tex, float layer) {
-	if(pos.size() != tex.size()) {
-		return pos;
-	}
-	for(unsigned i=0; i<pos.size(); i++) {
-		pos[i].tex = tex[i];
-	}
-
-	return pos;
-}
-
-const std::vector<Vertex2d> joinCoords(std::vector<glm::vec2> pos, std::vector<Vertex2d> tex, float layer) {
-	if(pos.size() != tex.size()) {
-		return tex;
-	}
-	for(unsigned i=0; i<pos.size(); i++) {
-		tex[i].pos = pos[i];
-	}
-
-	return tex;
-}
-
 Polygon& Polygon::mergeBasis(glm::vec2 lower_left, glm::vec2 upper_right) {
-	for(Vertex2d& v : vertices) {
-		v.tex *= (upper_right - lower_left);
-		v.tex += lower_left;
-	}
+	glm::mat4 new_basis = glm::translate(glm::mat4(1), glm::vec3(lower_left, 0)); // Move from the original origin to the new origin (lower left corner of basis)
+	new_basis = glm::scale(new_basis, glm::vec3(upper_right - lower_left, 1)); // Scale to the texBasis' size
+	basis_transform = basis_transform * new_basis;
 	return *this;
 }
 
 Polygon& Polygon::unmergeBasis(glm::vec2 lower_left, glm::vec2 upper_right) {
-	for(Vertex2d& v : vertices) {
-		v.tex -= lower_left;
-		v.tex /= (upper_right - lower_left);
-	}
+	glm::mat4 new_basis = glm::translate(glm::mat4(1), glm::vec3(lower_left, 0)); // Move from the original origin to the new origin (lower left corner of basis)
+	new_basis = glm::scale(new_basis, glm::vec3(upper_right - lower_left, 1)); // Scale to the texBasis' size
+	basis_transform = basis_transform * glm::inverse(new_basis);
 	return *this;
 }
 
